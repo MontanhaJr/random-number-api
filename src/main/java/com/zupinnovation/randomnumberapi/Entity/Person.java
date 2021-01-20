710c23652fef4d754386b00439063d82846ea26b
Mauricio Montanha Junior <mauriciomontanhajunior@gmail.com>
2021-01-20 00:31:56 -0300

    Entities Added

---
+++ src/main/java/com/zupinnovation/randomnumberapi/Entity/Person.java
@@ -0,0 +1,30 @@
+package com.zupinnovation.randomnumberapi.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<RandomNumber> numbers;
+
+}
