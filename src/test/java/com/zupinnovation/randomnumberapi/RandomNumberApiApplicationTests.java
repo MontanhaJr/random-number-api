342aec4403bb47cdb8529122c7bda684d5f97ab2
Mauricio Montanha Junior <mauriciomontanhajunior@gmail.com>
2021-01-19 22:06:32 -0300

    first commit

---
+++ src/test/java/com/zupinnovation/randomnumberapi/RandomNumberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zupinnovation.randomnumberapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomNumberApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
